4931f6de0c4a82625edbf988a66ea9b6302be23c
Rugved <rugvedabhyankar24@gmail.com>
2026-02-17 02:29:29 +0530

    Cors errro fix

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const DEFAULT_ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://signalxfrontend.vercel.app'
 ];
 
 const configuredOrigins = String(process.env.CORS_ALLOWED_ORIGINS || '')
